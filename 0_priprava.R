6fabd77f2dccbf5c2a34c1eee1a28b424457fd14
Petr Bouchal <pbouchal@gmail.com>
2020-11-04 23:46:33 +0100

    prep skript

---
+++ 0_priprava.R
@@ -0,0 +1,19 @@
+install.packages("rmarkdown")
+install.packages("tidyverse")
+install.packages("Rcpp")
+install.packages("rprojroot")
+install.packages("RCzechia")
+
+# tf <- tempfile(fileext = ".zip")
+#
+# download.file("https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip",
+#               destfile = tf)
+#
+# writeLines(
+#   readLines(
+#     unz(tf, filename = "2020_CZ_Region_Mobility_Report.csv")
+#   ),
+#   "data-zdroje/2020_CZ_Region_Mobility_Report.csv"
+# )
+#
+# read_csv("data-zdroje/2020_CZ_Region_Mobility_Report.csv")
