1888300f3acafe8d48729b047f197d18cbfc87d2
Petr Bouchal <pbouchal@gmail.com>
2020-11-18 10:07:54 +0100

    stazeni dat

--- 0_priprava.R
+++ 0_priprava.R
@@ -4,16 +4,17 @@ install.packages("Rcpp")
 install.packages("rprojroot")
 install.packages("RCzechia")
 
-# tf <- tempfile(fileext = ".zip")
-#
-# download.file("https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip",
-#               destfile = tf)
-#
-# writeLines(
-#   readLines(
-#     unz(tf, filename = "2020_CZ_Region_Mobility_Report.csv")
-#   ),
-#   "data-zdroje/2020_CZ_Region_Mobility_Report.csv"
-# )
-#
-# read_csv("data-zdroje/2020_CZ_Region_Mobility_Report.csv")
+library(tidyverse)
+tf <- tempfile(fileext = ".zip")
+
+download.file("https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip",
+              destfile = tf)
+
+writeLines(
+  readLines(
+    unz(tf, filename = "2020_CZ_Region_Mobility_Report.csv")
+  ),
+  "data-zdroje/2020_CZ_Region_Mobility_Report.csv"
+)
+
+read_csv("data-zdroje/2020_CZ_Region_Mobility_Report.csv")
